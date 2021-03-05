09c1f1d06df6d3e9b5754b641213c9d9c16fe1b5
Thilina FONSEKA <thilina.fonseka@insead.edu>
2021-03-05 22:17:36 +0800

    backend code implementation for itineray endpoint

--- server/index.js
+++ server/index.js
@@ -1,11 +1,27 @@
-const express = require('express')
-const app = express()
-const port = 8080
+const express = require('express');
+const app = express();
+const cors=require('cors');
+const fs = require('fs');
+const itineraries = require('./itineraries/index');
+app.use(cors());
+app.use(express.static(__dirname));
+
+
+app.use(express.urlencoded({ extended: false }))
+  .use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+// app.get('/itineraries',cors(),itineraries.getItineraries);
+app.get('/itineraries',(req, res) => {
+  let rawdata = fs.readFileSync('flights.json');
+  let data = JSON.parse(rawdata);
+  console.log(data.itineraries);
+  res.send({"status": "success", "data": data.itineraries})
+});
+
+app.listen(process.env.PORT || 4000, function(){
+  console.log('Your node js server is running');
+});
